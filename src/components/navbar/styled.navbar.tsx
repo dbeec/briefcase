d36f3dc821aa6184ed724ed58994a7c32419592a
beecodes <dev.beecodes@gmail.com>
2023-11-02 20:55:30 -0500

    updated

--- src/components/navbar/styled.navbar.tsx
+++ src/components/navbar/styled.navbar.tsx
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 import Link from 'next/link'
 
 export const ContentNavbar = styled.div`
-  background-color: #fff;
   height: 10vh;
   display: flex;
   justify-content: center;

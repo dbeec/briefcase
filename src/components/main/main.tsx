132d1164ab51f2263b9996ad089a9386cb6b4568
Johan Díaz <johan.diaz@etfundacion.org>
2023-11-03 16:03:54 -0500

    updated

--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -9,22 +9,24 @@ import {
   SocialNetworks,
   TextPersonal,
 } from "./styled.main";
-import {BiLogoFacebook} from 'react-icons/bi'
-import {BiLogoInstagram} from 'react-icons/bi'
-import {BiLogoGithub} from 'react-icons/bi'
-import {RiTwitterXFill} from 'react-icons/ri'
-import {FaLinkedinIn} from 'react-icons/fa'
+import { BiLogoFacebook } from "react-icons/bi";
+import { BiLogoInstagram } from "react-icons/bi";
+import { BiLogoGithub } from "react-icons/bi";
+import { RiTwitterXFill } from "react-icons/ri";
+import { FaLinkedinIn } from "react-icons/fa";
 const Main = () => {
   return (
     <>
       <ContentMain>
+
         <MyProfile>
+
           <ContentProfilePic>
             <ProfileMain>profile here</ProfileMain>
           </ContentProfilePic>
 
           <PersonalInformation>
-            <H1>Hello, I{"'"}m Johan 💚💛</H1>
+            <H1>Hello, I{"'"}m Johan</H1>
             <H2>Developer Frontend Junior</H2>
             <TextPersonal>✈️ love travel to the world</TextPersonal>
             <TextPersonal>🏍️ i{"'"}ve a passion for motorcycles</TextPersonal>
@@ -32,16 +34,17 @@ const Main = () => {
             <TextPersonal>💻 love with de code</TextPersonal>
 
             {/* iconos de mis redes sociales */}
-          <SocialNetworks>
-            <BiLogoFacebook />
-            <BiLogoInstagram />
-            <BiLogoGithub />
-            <RiTwitterXFill />
-            <FaLinkedinIn />
-          </SocialNetworks>
+            <SocialNetworks>
+              <BiLogoFacebook />
+              <BiLogoInstagram />
+              <BiLogoGithub />
+              <RiTwitterXFill />
+              <FaLinkedinIn />
+            </SocialNetworks>
           </PersonalInformation>
 
         </MyProfile>
+        
       </ContentMain>
     </>
   );

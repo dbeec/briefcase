6b15883ae7e9ef02930c55c31a7e3e7dcdb50194
Johan Díaz <johan.diaz@etfundacion.org>
2023-11-04 10:47:07 -0500

    updated

---
+++ src/components/profile/profile.tsx
@@ -0,0 +1,76 @@
+import {
+  ContentMain,
+  ContentProfilePic,
+  H1,
+  H2,
+  MyProfile,
+  PersonalInformation,
+  Picture,
+  ProfileMain,
+  SocialNetworks,
+  Span,
+  TextPersonal
+} from "./styled.profile";
+import Link from 'next/link'
+import { BiLogoFacebook } from "react-icons/bi";
+import { BiLogoInstagram } from "react-icons/bi";
+import { BiLogoGithub } from "react-icons/bi";
+import { RiTwitterXFill } from "react-icons/ri";
+import { FaLinkedinIn } from "react-icons/fa";
+
+// aqui exporto la imagen automaticamente de la carpeta public
+const img1 = '/Me.jpg';
+
+const Profile = () => {
+  return (
+    <>
+      <ContentMain>
+
+        <MyProfile>
+
+          <ContentProfilePic>
+            <ProfileMain>
+              <Picture src={img1} alt="image"/>
+            </ProfileMain>
+          </ContentProfilePic>
+
+          <PersonalInformation>
+            <H1>Hello, <Span>I{"'"}m Johan</Span></H1>
+            <H2>Developer Frontend Junior</H2>
+            <TextPersonal>✈️ love travel to the world</TextPersonal>
+            <TextPersonal>🏍️ i{"'"}ve a passion for motorcycles</TextPersonal>
+            <TextPersonal>☕ coffe and more coffe</TextPersonal>
+            <TextPersonal>💻 love with de code</TextPersonal>
+
+            {/* iconos de mis redes sociales */}
+            <SocialNetworks>
+              <Link title='Facebook' href='https://www.facebook.com/alleeschmidt' target='_blank'>
+              <BiLogoFacebook />
+              </Link>
+
+              <Link title='Instagram' href='https://www.instagram.com/dev.beecodes/' target='_blank'>
+              <BiLogoInstagram />
+              </Link>
+
+              <Link title='GitHub' href='https://github.com/dbeec' target='_blank'>
+              <BiLogoGithub />
+              </Link>
+
+              <Link title='Twitter' href='https://twitter.com/imjdiazq' target='_blank'>
+              <RiTwitterXFill />
+              </Link>
+
+              <Link title='LinkedIn' href='https://www.linkedin.com/in/johan-d%C3%ADaz-ba83ab28b/' target='_blank'>
+              <FaLinkedinIn />
+              </Link>
+            </SocialNetworks>
+          </PersonalInformation>
+
+        </MyProfile>
+
+      </ContentMain>
+    </>
+  );
+};
+
+export default Profile;
---
+++ src/components/profile/styled.profile.tsx
@@ -0,0 +1,109 @@
+import styled from 'styled-components';
+
+interface PictureProps {
+  src: string;
+  alt: string;
+}
+
+export const ContentMain = styled.div`
+  /* background-color: #222; */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 90vh;
+  padding: 25px;
+  gap: 20px;
+`;
+
+export const MyProfile = styled.div`
+  /* background: #222; */
+  display: flex;
+  align-items: center;
+  width: auto;
+  height: auto;
+  gap: 30px;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    width: auto;
+  }
+`;
+
+export const SocialNetworks = styled.div`
+  /* background-color: red; */
+  display: flex;
+  width: 100%;
+  font-size: 2rem;
+  gap: 20px;
+  margin-top: 20px;
+  
+  @media (max-width: 768px) {
+    /* background-color: red; */
+    width: 100%;
+    height: 8vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 20px;
+  }
+`;
+
+export const ContentProfilePic = styled.div`
+  
+`;
+export const ProfileMain = styled.div`
+  background: #eee;
+  display: flex; //eliminar estilo
+  justify-content: center; //eliminar estilo
+  align-items: center;  //eliminar estilo
+  width: 250px;
+  height: 250px;
+  background: transparent;
+  `;
+
+export const Picture = styled.img<PictureProps>`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  border-radius: 50%;
+  padding: 6px;
+  /* box-sizing: border-box; */
+  background: linear-gradient(#118333, #B5AC49)
+  
+`;
+
+export const PersonalInformation = styled.div`
+  @media (max-width: 768px) {
+  }
+`;
+
+export const H1 = styled.h1`
+  font-size: 3rem;
+
+  @media (max-width: 768px) {
+    text-align: center;
+  }
+`;
+
+export const Span = styled.span`
+  background: #118333;
+background: -webkit-linear-gradient(to right, #B5AC49, #118333);
+background: linear-gradient(to right, #B5AC49, #118333);
+-webkit-background-clip: text;
+background-clip: text;
+color: transparent;
+`;
+
+export const H2 = styled.h2`
+  @media (max-width: 768px) {
+    text-align: center;
+  }
+`;
+
+export const TextPersonal = styled.p`
+  @media (max-width: 768px) {
+    margin-left: 10px;
+    padding: 4px;
+  }
+`;
